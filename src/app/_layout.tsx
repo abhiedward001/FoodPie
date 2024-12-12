0886bba5dfb15ecec88be3285210a035d339fbb5
Abhishek Jaiswal <iam001abhi@gmail.com>
2024-12-12 16:24:54 +0530

    feat: initial codebase setup for application

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,13 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useColorScheme } from '../components/useColorScheme';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 
-import { useColorScheme } from '@/components/useColorScheme';
-
+ 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
@@ -23,7 +23,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
 
